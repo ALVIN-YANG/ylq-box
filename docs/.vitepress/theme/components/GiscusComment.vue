f8b27a206169fe8f1d16b14d3015f20a0fdb2109
杨卢青 <luqing.yang@hoorii.io>
2024-06-07 18:09:38 +0800

    feat: add giscus

---
+++ docs/.vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="margin-top: 24px">
+    <Giscus
+      id="comments"
+      repo="haovei/site"
+      repoid="R_kgDOLx5gaw"
+      category="General"
+      categoryid="DIC_kwDOLx5ga84Cf7S3"
+      mapping="pathname"
+      term="Welcome to giscus!"
+      reactionsenabled="1"
+      emitmetadata="0"
+      inputposition="top"
+      loading="lazy"
+      :theme="isDark ? 'dark' : 'light'"
+      :key="route.path"
+    ></Giscus>
+  </div>
+</template>
+
+<script setup>
+import Giscus from "@giscus/vue";
+import { useRoute, useData } from "vitepress";
+
+const route = useRoute();
+const { isDark } = useData();
+</script>
